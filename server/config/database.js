4f289020dba0e1a6f45d811828eb1c9bc2cd35a3
Shlomo Ariel <shlomoa@balink.net>
2018-10-13 10:59:52 +0300

    use heroku config var

--- server/config/database.js
+++ server/config/database.js
@@ -1,6 +1,5 @@
 module.exports = {
   secret: 'devdacticIsAwesome',
   database:
-    process.env.DATABASE ||
-    'mongodb://shlomoariel:a1345678@ds255260.mlab.com:55260/user-management-template',
+    process.env.DATABASE
 };
