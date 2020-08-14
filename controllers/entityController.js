c57f5df0946dda0bf6a0c9eb6548fe6df3368173
Shlomo Ariel <shlomoa@balink.net>
2020-08-14 18:54:06 +0300

    test no async

--- controllers/entityController.js
+++ controllers/entityController.js
@@ -27,10 +27,10 @@ router.get('/api/getEntity/:id', passport.authenticate('jwt', { session: false }
         });
 });
 
-router.get('/api/getEntities', passport.authenticate('jwt', { session: false }), async function (req, res, next) {
+router.get('/api/getEntities', passport.authenticate('jwt', { session: false }), function (req, res, next) {
     var entityName = req.query.entityName
     if(entityName){
-        var entities = await getEntitiesBy(entityName)
+        var entities = getEntitiesBy(entityName)
         if(entities){
             res.json(entities);            
         }
