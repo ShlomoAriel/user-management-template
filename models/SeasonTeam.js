7789a4de56387fe9ae26e1e30404d07e3139a5ba
Shlomo Ariel <shlomoa@balink.net>
2020-08-16 11:52:13 +0300

    update model

--- models/SeasonTeam.js
+++ models/SeasonTeam.js
@@ -5,6 +5,8 @@ var seasonTeamSchema = new Schema({
     team:{ type: Schema.ObjectId, ref: 'Team', required: true },
     season:{ type: Schema.ObjectId, ref: 'Season', required: true },
     players: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Player' }]
-});
+}, {
+    usePushEach : true
+  });
 var SeasonTeam = mongoose.model('SeasonTeam', seasonTeamSchema);
 module.exports = SeasonTeam;
