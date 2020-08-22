e76007f69baf844301b92f33277a7fc85f91aa5b
Shlomo Ariel <shlomoa@balink.net>
2020-08-22 16:48:17 +0300

    naming and logic improvements

--- controllers/entityController.js
+++ controllers/entityController.js
@@ -4,8 +4,8 @@ const passport = require('passport');
 
 router.post('/api/addEntity', passport.authenticate('jwt', { session: false }), addEntity);
 
-router.get('/api/getEntity/:id', passport.authenticate('jwt', { session: false }), function (req, res, next) {
-    var entity = getEntityModel(req.query.entityName, next)   
+router.get('/api/get/:entityName/:id', passport.authenticate('jwt', { session: false }), function (req, res, next) {
+    var entity = getEntityModel(req.params.entityName, next)   
     entity.findOne({ _id: req.params.id })
         .exec(function (err, entity) {
             if (err) {
@@ -18,22 +18,25 @@ router.get('/api/getEntity/:id', passport.authenticate('jwt', { session: false }
         });
 });
 
-router.get('/api/getEntities', passport.authenticate('jwt', { session: false }), async function (req, res, next) {
-    var entityName = req.query.entityName
+router.get('/api/get/:entityName', passport.authenticate('jwt', { session: false }), async function (req, res, next) {
+    var entityName = req.params.entityName
     if(entityName){
         var entities = await getEntitiesBy(entityName, next)
         if(entities){
-            res.json(entities);            
+            res.json({"result":entities});            
         }
     }
 });
 
-router.put('/api/updateEntity', passport.authenticate('jwt', { session: false }), function (req, res, next) {
-    var entity = getEntityModel(req.body.entityName, next)
-    if (req.body.entity._id == null || req.body.entity._id == undefined){
+router.put('/api/upsert/:entityName/:id?', passport.authenticate('jwt', { session: false }), function (req, res, next) {
+    if (!req.params.entityName) {
+        next('Error updating Entity\n' + err);
+    }
+    var entity = getEntityModel(req.params.entityName, next)
+    if (req.params.id == null || req.params.id == undefined){
         addEntity(req, res, next)
     } else {
-        const filter = { _id: req.body.entity._id };
+        const filter = { _id: req.params.id };
         const update = req.body.entity;
         entity.findOneAndUpdate( filter, update, { 
             new: true,
@@ -50,7 +53,7 @@ router.put('/api/updateEntity', passport.authenticate('jwt', { session: false })
     }
 });
 
-router.delete('/api/deleteEntity/:id/:entityName', passport.authenticate('jwt', { session: false }), function (req, res, next) {
+router.delete('/api/delete2/:entityName/:id', passport.authenticate('jwt', { session: false }), function (req, res, next) {
     if (!req.params.entityName || !req.params.id) {
         next('Error deleting Entity\n' + err);
     }
@@ -62,26 +65,35 @@ router.delete('/api/deleteEntity/:id/:entityName', passport.authenticate('jwt', 
                 next('Error deleting Entity\n' + err);
             }
             else {
-                res.send(204);
+                if(newEntity == null) {
+                    res.status(404).json({status:404})
+                } else {
+                    res.json(newEntity)
+                    res.status(200)
+                }
+                
             }
         });
 });
 
-router.get('/api/findEntityLike', passport.authenticate('jwt', { session: false }), function (req, res, next) {
-    var entity = getEntityModel(req.body.entityName, next)
-    entity.find(function (err, entities) {
+router.get('/api/find/:entityName', passport.authenticate('jwt', { session: false }), function (req, res, next) {
+    var entity = getEntityModel(req.params.entityName, next)
+    if(req.query.q == null || req.query.q == undefined || req.query.q == '') {
+        res.status(400).json({status:400})
+        return 
+    }
+
+    entity.find({'name' : new RegExp(req.query.q, 'i')}, function (err, entities) {
         if (err) {
             next('find no good' + err);
         }
         else {
-            var searchString = req.param('searchString');
-            let objectArray = _.filter(entities, function (o) {
-                return o.name.includes(searchString);
-            });
-            res.json(objectArray);
+            
+            res.json(entities);
         }
     });
 });
+
 async function getEntitiesBy(name, next){
     var entity = getEntityModel(name, next)
     return entity.find(function (err, entities) {
@@ -90,6 +102,18 @@ async function getEntitiesBy(name, next){
         }
         else {
             return(entities)
+        }
+    })
+}
+
+async function getEntitiesById(name, id, next){
+    var entity = getEntityModel(name, next)
+    return entity.findOne({ _id: id }, (err, entity) => {
+        if (err) {
+            next('getEntity: ' + name + ' ' + id + ' no good' + err)
+        }
+        else {
+            return entity
         }
     })
 }
@@ -109,7 +133,7 @@ function getEntityModel(name, next){
 }
 
 function addEntity(req, res, next) {
-    var entityModel = getEntityModel(req.body.entityName, next)
+    var entityModel = getEntityModel(req.params.entityName, next)
     var entity = new entityModel(req.body.entity)
     entity.save((err, newEntity) => {
         if (err) {
@@ -119,4 +143,16 @@ function addEntity(req, res, next) {
     });
 }
 
-    module.exports = router
+router.put('/api/addToEntity', passport.authenticate('jwt', { session: false }), async function (req, res, next) {
+    var mainEntity = await getEntitiesById(req.body.secondaryEntity.name, req.body.secondaryEntity.id)
+    console.log(entity[req.body.collectionName])
+    mainEntity[req.body.collectionName].push(req.body.mainEntity)
+    mainEntity.save(function(err, doc){
+        if(err) {
+            res.status(400).send(err.stack)
+        }
+        res.status(200).send(doc)
+    })
+})
+
+module.exports = router
