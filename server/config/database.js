e386f2c18b14eae1eccc9a2d78d543978dd68f35
Shlomo Ariel <shlomoa@balink.net>
2021-05-09 08:09:35 +0300

    u[date env

--- server/config/database.js
+++ server/config/database.js
@@ -2,5 +2,5 @@ module.exports = {
   secret: 'devdacticIsAwesome',
   database:
     process.env.DATABASE ||
-    'mongodb://shlomoariel:a1345678@user-management-templat-shard-00-00.qseuh.mongodb.net:27017,user-management-templat-shard-00-01.qseuh.mongodb.net:27017,user-management-templat-shard-00-02.qseuh.mongodb.net:27017/user-management-template?ssl=true&replicaSet=atlas-i8gv06-shard-0&authSource=admin&retryWrites=true&w=majority'
+    'mongodb+srv://shlomoariel:a1345678@user-management-templat.qseuh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
 };
